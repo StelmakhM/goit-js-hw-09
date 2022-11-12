4e506820da3078a93873715d30bea3c1781f31e2
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-11-12 23:27:38 +0200

    9.1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+	startBtn: document.querySelector("[data-start]"),
+	stopBtn: document.querySelector("[data-stop]"),
+	body: document.body,
+};
 
+const DELAY = 1000;
+let timerId;
+
+refs.startBtn.addEventListener("click", onStartBtnClick);
+refs.stopBtn.addEventListener("click", onStopBtnClick);
+
+function onStartBtnClick(e) {
+	e.target.setAttribute("disabled", "");
+	timerId = setTimeout(function changeBodyColor() {
+		refs.body.style.backgroundColor = getRandomHexColor();
+		timerId = setTimeout(changeBodyColor, DELAY);
+	}, DELAY);
+
+}
+
+function onStopBtnClick(e) {
+	clearTimeout(timerId);
+	refs.startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
