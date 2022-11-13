75e22fbc4b53ba4589d2ffee00593e79703925ae
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-11-13 22:25:09 +0200

    9.3 task done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from "notiflix";
+
+const refs = {
+    form: document.querySelector(".form"),
+};
+
+refs.form.addEventListener("submit", onFormSubmit);
+const { amount, delay, step } = refs.form.elements;
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    let delayValue = Number(delay.value);
+    const amountValue = Number(amount.value);
+    const stepValue = Number(step.value);
+
+    for (let i = 1; i <= amountValue; i += 1) {
+        createPromise(i, delayValue)
+            .then(({ position, delay }) => {
+                Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            })
+            .catch(({ position, delay }) => {
+                Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+            });
+
+        delayValue += stepValue;
+    }
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    return new Promise((resolve, reject) => {
+        const shouldResolve = Math.random() > 0.5;
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve({ position, delay });
+            } else {
+                reject({ position, delay });
+            }
+        }, delay);
+    });
 }
